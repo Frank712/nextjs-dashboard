b7811ba5f5db0bba0a82c0d5558cd17bf15d6c70
Francisco Lino <franksuriel_7@hotmail.com>
2024-03-18 10:41:34 -0600

    ADD my counter

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,7 +1,15 @@
+import { CartCounter } from "@/app/shopping-cart";
+
+export const metadata = {
+  title: "Counter Page",
+  description: "A simple counter",
+};
+
 export default function CounterPage() {
   return (
-    <div>
-      <h1>Hello CounterPage</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito de compras</span>
+      <CartCounter value={144} />
     </div>
   );
 }
