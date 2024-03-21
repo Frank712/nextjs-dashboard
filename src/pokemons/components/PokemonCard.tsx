79983805a2cb73b84ee8a6945c7b72633a86e032
Francisco Lino <franksuriel_7@hotmail.com>
2024-03-21 12:01:34 -0600

    ADD static parameters

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -14,7 +14,7 @@ export const PokemonCard = ({ pokemon }: Props) => {
       <div className="bg-white rounded overflow-hidden shadow-lg">
         <div className="flex flex-col items-center justify-center text-center p-6 bg-gray-800 border-b">
           <Image
-            key={pokemon.id}
+            key={id}
             src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
             height={100}
             width={100}
@@ -26,7 +26,7 @@ export const PokemonCard = ({ pokemon }: Props) => {
           </p>
           <div className="mt-5">
             <Link
-              href={`/dashboard/pokemon/${id}`}
+              href={`/dashboard/pokemons/${name}`}
               className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
             >
               Más información
